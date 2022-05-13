e036fadd26ccc5672cf29f35142f1412efe7aa9e
Emilio kariuki <emilio113kariuki@gmail.com>
2022-05-13 21:56:06 +0300

    added a flutter toast to the application

--- lib/data/fetchData.dart
+++ lib/data/fetchData.dart
@@ -1,15 +1,24 @@
 import 'dart:convert';
-
+import "package:fluttertoast/fluttertoast.dart";
 import 'package:get_controller/model/get_model.dart';
 import "package:http/http.dart" as http;
+import "package:flutter/material.dart";
 
 class RemoteServices {
   static var client = http.Client();
   static Future<List<Welcome>> fetchData() async {
-    var response = await client.get(Uri.parse(
-        "https://lucifer-quotes.vercel.app/api/quotes/500"));
+    var response = await client
+        .get(Uri.parse("https://lucifer-quotes.vercel.app/api/quotes/500"));
 
     if (response.statusCode == 200) {
+      Fluttertoast.showToast(
+          msg: "Information fetched successfully",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Color.fromARGB(255, 9, 196, 43),
+          textColor: Colors.white,
+          fontSize: 16.0);
       print("Information fetched successfully");
       var json = response.body;
       // print(jsonDecode(json));
